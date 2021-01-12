c900273e35d9f8dd4b7fa95f006382eb3bb426c0
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2021-01-12 15:52:05 +0530

    Prime Number

---
+++ Primef.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Primef {
+    public static void main(String[]args){
+        int t=5;
+        for(int j=t;j>0;j--)
+        {
+
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter the number: ");
+        int n=scan.nextInt();
+        int count=0;
+
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0){
+                count=1;
+                break;
+            }
+            else
+               count=0;
+            
+        }
+        if(n==1)
+        System.out.println(" 1 is neither prime or not prime");
+        else if(count==1)
+        System.out.println("not prime");
+        else
+        System.out.println("prime");
+    }
+
+
+    }
+    
+}
